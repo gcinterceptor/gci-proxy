773777bf3fe4f2bde2fb4ff5d23a4271dd6a379f
Daniel Fireman <danielfireman@gmail.com>
2018-11-13 15:30:38 -0300

    Small fix in nopserver

--- nopserver/main.go
+++ nopserver/main.go
@@ -12,5 +12,5 @@ var port = flag.Int("port", 8080, "Server port to listen")
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {})
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
