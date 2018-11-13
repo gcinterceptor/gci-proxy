e6d1e0410865b9498bb4c980f7658197b4391736
Daniel Fireman <danielfireman@gmail.com>
2018-11-13 15:29:29 -0300

    Adds a server which does nothing

---
+++ nopserver/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var port = flag.Int("port", 8080, "Server port to listen")
+
+func main() {
+	flag.Parse()
+	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {})
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
