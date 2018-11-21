11e70583b7fecc0466180c5e7821e9b7b9f2e2af
Daniel Fireman <danielfireman@gmail.com>
2018-11-21 12:52:03 -0300

    Adding gci to nopserver

--- nopserver/main.go
+++ nopserver/main.go
@@ -5,12 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gcinterceptor/gci-go/httphandler"
 )
 
-var port = flag.Int("port", 8080, "Server port to listen")
+var (
+	port      = flag.Int("port", 8080, "Server port to listen")
+	useGCI    = flag.Bool("gci", false, "Whether to use GCI.")
+	nopHandle = func(http.ResponseWriter, *http.Request) {}
+)
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {})
+	if *useGCI {
+		http.Handle("/", httphandler.GCI(nopHandle))
+	} else {
+		http.HandleFunc("/", nopHandle)
+	}
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
