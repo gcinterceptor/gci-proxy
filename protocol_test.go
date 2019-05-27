30eee6516470fb8e98777b90cbf6c6680ae5bfa4
Daniel Fireman <danielfireman@gmail.com>
2019-05-27 13:52:00 -0300

    More checks to SheddingThreshold

--- protocol_test.go
+++ protocol_test.go
@@ -7,23 +7,40 @@ import (
 )
 
 func TestSheddingThreshold_NextValue(t *testing.T) {
-	is := is.New(t)
-	s := newSheddingThreshold(1, 1000)
+	t.Run("Sanity", func(t *testing.T) {
+		is := is.New(t)
+		s := newSheddingThreshold(1, 1000)
 
-	curr := s.val
-	is.True(s.val > s.min) // CurrVal should be bigger than minimum.
-	is.True(s.val < s.max) // CurrVal should be bigger than minimum.
+		curr := s.val
+		is.True(s.val > s.min) // CurrVal should be bigger than minimum.
+		is.True(s.val < s.max) // CurrVal should be bigger than minimum.
 
-	next := s.NextValue()
+		next := s.NextValue()
+		is.True(next > s.min) // NextValue should be bigger than minimum.
+		is.True(next < s.max) // NextValue should be bigger than minimum.
 
-	is.True(next > s.min) // NextValue should be bigger than minimum.
-	is.True(next < s.max) // NextValue should be bigger than minimum.
-	is.True(next > curr)  // Next should increase until it reaches the maximum.
+		is.True(next > curr) // Next should increase until it reaches the maximum.
+	})
 
-	next1 := s.NextValue()
-	next2 := s.NextValue()
+	t.Run("MustDecreaseIfGreaterThanMaximum", func(t *testing.T) {
+		is := is.New(t)
+		s := newSheddingThreshold(1, 1000)
+		s.max = s.val + 1
+		v := s.val
+		next := s.NextValue()
+		is.True(next < v)
+		is.True(next < s.max)
+	})
 
-	is.True(next2 < next1) // Next should decrease when reached the maximum.
+	t.Run("MustIncreaseIfLessThanMinimum", func(t *testing.T) {
+		is := is.New(t)
+		s := newSheddingThreshold(1, 1000)
+		s.min = s.val - 1
+		v := s.val
+		next := s.NextValue()
+		is.True(next > v)
+		is.True(next > s.min)
+	})
 }
 
 func TestSheddingThreshold_GC(t *testing.T) {
